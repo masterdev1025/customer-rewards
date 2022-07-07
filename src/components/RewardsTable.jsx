6b3158657a9e97280925b74a19032fcb98859d5d
John Rowe <john.rowe.devmaster@gmail.com>
2022-07-07 15:05:02 -0500

    show the transaction data

--- src/components/RewardsTable.jsx
+++ src/components/RewardsTable.jsx
@@ -4,7 +4,32 @@ import useFetch from '../hooks/useFetch'
 const RewardsTable = () => {
     const transactions = useFetch('/transactions');
     return(
-        <></>
+        <>
+            <div className = "wrapper">
+                <table>
+                    <thead>
+                        <tr>
+                            <td>ID</td>
+                            <td>Customer</td>
+                            <td>Transaction Amount</td>
+                            <td>Rewards</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            transactions?.data && transactions?.data.map((t) => (
+                                <tr key = {t.id}>
+                                    <td>{t.id}</td>
+                                    <td>{t.customer_name}</td>
+                                    <td>{t.amout}</td>
+                                    <td>{t.amout}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </>
     )
 }
 
